5eccd81eb17c5e591214566f6e4345d45729db55
rifat977 <itscrifat5147@gmail.com>
2024-05-08 14:01:44 +0600

    initial

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def send_sheet(message):
 
 @bot.message_handler(commands=['gpt'])
 def drive_link(message):
-    text = "ji vai bolen.."
+    text = "Say something.."
     sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
     bot.register_next_step_handler(sent_msg, voice_processing)
 
